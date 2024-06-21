493a27d2b137a7e2d5aea871f3a338c53d1b50ec
Piyush Karmacharya <piyushkarmacharya65@gmail.com>
2024-06-21 10:36:33 +0545

    used router and used parms

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 import './components/NavBar'
 import NavBar from './components/NavBar';
 import Column from './components/Column';
 import Alert from './components/Alert';
 import { useState } from 'react';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import User from './components/User';
+
 
 function App() {
+  
+  
   const [themee,setThemee]=useState("light");
   const [alert,setAlert]=useState(null);
   const showAlert=(msg,alertType)=>{
@@ -23,7 +29,7 @@ function App() {
   const changeThemee=(event)=>{
     document.body.style.backgroundColor = themee === 'light' ? '#212529' : 'white';
     document.body.style.color = themee === 'light' ? 'white' : 'black';
-    if(themee=='light'){
+    if(themee==='light'){
       showAlert('Converted to Dark mode','success');
     }else{
       showAlert('Converted to Light mode','success');
@@ -32,10 +38,29 @@ function App() {
     setThemee(themee === 'light' ? 'dark' : 'light');
     
   }
+  const router=createBrowserRouter([
+    {
+      path:"/",
+      element:<><NavBar themee={themee} changeThemee={changeThemee}/><Home/></>
+    },
+    {
+      path:"/column",
+      element:<><NavBar themee={themee} changeThemee={changeThemee}/><Column/></>
+    },
+    {
+      path:"/about",
+      element:<><NavBar themee={themee} changeThemee={changeThemee}/><About/></>
+    },
+    {
+      path:"/user/:username",
+      element:<><NavBar themee={themee} changeThemee={changeThemee}/><User/></>
+    }
+  ]);
   return (
-    <><NavBar themee={themee} changeThemee={changeThemee}></NavBar>
+    <>
+   
+    <RouterProvider router={router}/>
     <Alert alert={alert} ></Alert>
-    <Column></Column>
     
     </>
     
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,6 +1,5 @@
-import React, { useState } from 'react'
 import PropTypes from 'prop-types';
-
+import { Link } from 'react-router-dom';
 export default function NavBar(props) {
   
   return (
@@ -14,7 +13,7 @@ export default function NavBar(props) {
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">Home</a>
+          <Link className="nav-link active" aria-current="page" to="/">Home</Link>
         </li>
         <li className="nav-item">
           <a className="nav-link" href="/">Get Membership</a>
@@ -30,6 +29,12 @@ export default function NavBar(props) {
             <li><a className="dropdown-item" href="/">Something else here</a></li>
           </ul>
         </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/column">Column</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="nav-link" to="/about">About</Link>
+        </li>
        
       </ul>
       {/* <form className="d-flex" role="search">
@@ -38,7 +43,7 @@ export default function NavBar(props) {
       </form> */}
       <div className="form-check form-switch">
   <input className="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" onClick={props.changeThemee}></input>
-  <label className="form-check-label" htmlFor="flexSwitchCheckDefault" style={{color:props.themee=='light'?'black':'white'}}>{props.themee=='dark'?'light':'dark'}</label>
+  <label className="form-check-label" htmlFor="flexSwitchCheckDefault" style={{color:props.themee==='light'?'black':'white'}}>{props.themee==='dark'?'light':'dark'}</label>
 </div>
       
 
